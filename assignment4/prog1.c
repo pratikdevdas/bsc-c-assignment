07613c17d72a9601f99badf2ada4fca399902b13
revenant <dasdev.pratik@gmail.com>
2021-11-28 20:22:59 +0530

    asg4

--- assignment4/prog1.c
+++ assignment4/prog1.c
@@ -1,1 +1,17 @@
-prog1.c
+/* Using relational operators */
+#include <stdio.h>
+int main()
+{
+int x, y;
+
+ x = 10;
+ y = 25;
+ printf("Given x = %d, y = %d, \n", x, y);
+ printf("x >= y produces: %d\n", x >= y);
+ printf("x == y produces: %d\n", x == y);
+ printf("x < y produces: %d\n", x < y);
+ printf("y > x produces: %d\n", y > x);
+ printf("x != y - 18 produces: %d\n", x != y - 18);
+ printf("x + y != 35 produces: %d\n", x + y != 35);
+ return 0;
+ }
